ca631ac3b31f06c12cdc3ff486e081fa85a3f92d
KristenLeach <31492991+KristenLeach@users.noreply.github.com>
2018-03-22 13:28:08 -0700

    updated with corneal layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
+require './config/environment'
+require 'sinatra/flash'
 class ApplicationController < Sinatra::Base
 
     configure do
       set :public_folder, 'public'
       set :views, 'app/views'
+      register Sinatra::Flash
       enable :sessions
       set :session_secret, "password_security"
     end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,1 +1,31 @@
 class UserController < ApplicationController
+
+    get '/signup' do 
+        !logged_in? ? (erb :'/users/signup') : (redirect "/")
+    end 
+
+    get '/users/:slug' do
+        @user = User.find_by_slug(params[:slug])
+        erb :'users/account'
+    end
+
+    post '/signup' do
+        user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
+        !!user.save ? (session[:user_id] = user.id) && (redirect "/users/#{user.slug}") : flash[:error] = "Something went wrong. Please try again!" && (redirect "/signup")
+    end
+
+    get '/login' do
+        user = User.find_by(:username => params[:username])
+        !logged_in? ? (erb :'/users/login') : (redirect "/users/#{user.slug}")
+    end
+
+    post "/login" do
+        user = User.find_by(:username => params[:username])
+        !!user && user.authenticate(params[:password]) ? (session[:user_id] = user.id) && (redirect "/users/#{user.slug}") : (redirect "/signup")
+    end
+
+    get "/logout" do
+        !!logged_in? ? (session.destroy) && (redirect '/login') : (redirect "/")
+    end
+
+end
